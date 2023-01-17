22dd5f8aa3b42f6f6a355907a4e735d0241e40e1
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2023-01-17 17:52:16 -0300

    redimensionamento de imagens

---
+++ app.js
@@ -0,0 +1,75 @@
+const sharp = require('sharp');
+const compress_images = require('compress-images');
+const fs = require('fs');
+
+let path = process.argv[2];
+let width = Number(process.argv[3]);
+
+function resize(inputPath, outputPath, width) {
+  sharp(inputPath)
+    .rotate(90)
+    .resize({ width })
+    .toFile(outputPath, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Imagem redimensionada com sucesso!');
+        compress(outputPath, "./compressed/");
+      }
+    });
+}
+resize(path, './temp/output_resize.jpg', width);
+
+function compress(pathInput, outputPath) {
+  compress_images(
+    pathInput,
+    outputPath,
+    { compress_force: false, statistic: true, autoupdate: true },
+    false,
+    { jpg: { engine: 'mozjpeg', command: ['-quality', '60'] } },
+    { png: { engine: 'pngquant', command: ['--quality=20-50', '-o'] } },
+    { svg: { engine: 'svgo', command: '--multipass' } },
+    {
+      gif: { engine: 'gifsicle', command: ['--colors', '64', '--use-col=web'] },
+    },
+    function (error, completed, statistic) {
+      console.log('-------------');
+      console.log(error);
+      console.log(completed);
+      console.log(statistic);
+      console.log('-------------');
+
+      fs.unlink(pathInput, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(pathInput, ' Apagado');
+        }
+      });
+    },
+  );
+}
+
+
+
+
+
+
+// Essa parte não tem nada a ver com o código de redimensionamento. Isto é apenas teste de erro.
+/* function t() {
+  try {
+    teste();
+  } catch (err) {
+    console.log(err);
+  }
+}
+t()
+
+function teste() {
+  try {
+    throw new Error('Ocorreu um erro');
+  } catch (err) {
+    console.log(err);
+    throw new Error('Ai toma');
+  }
+} */
